8f8c6d181af1428ba1c1db03c9c360371e136809
toshi0383 <t.suzuki326@gmail.com>
2025-01-19 00:00:29 +0900

    chore: update Package.swift

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,32 @@
 mkdir CLIApp
 cd CLIApp
 swift package init --type executable --name CLIApp
+cat > Package.swift << EOF
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "CLIApp",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(
+            name: "CLIApp",
+            targets: ["CLIApp"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "CLIApp",
+            dependencies: [
+                .product(name: "Collections", package: "swift-collections")
+            ]
+        ),
+    ]
+)
+EOF
+
