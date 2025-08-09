c343711717429d348d041675226e0ec240230ba4
toshi0383 <t.suzuki326@gmail.com>
2025-08-10 01:14:10 +0900

    fix: bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,20 +3,14 @@ mkdir CLIApp
 cd CLIApp
 swift package init --type executable --name CLIApp
 cat > Package.swift << EOF
-// swift-tools-version:5.10
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "CLIApp",
-    platforms: [
-        .macOS(.v13)
-    ],
-    products: [
-        .executable(
-            name: "CLIApp",
-            targets: ["CLIApp"]
-        )
-    ],
+    platforms: [.macOS(.v14)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0")
     ],
